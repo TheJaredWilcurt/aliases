20051e24eb128b221be1f1bd6b1fed1f22bc9c88
The Jared Wilcurt <TheJaredWilcurt@users.noreply.github.com>
2022-12-08 15:43:39 -0500

    Update main.bat

--- main.bat
+++ main.bat
@@ -1,1 +1,25 @@
+: Use: main my-branch-name
+
+@echo off
+
+: If an argument is passed in
+IF NOT "%1"=="" (
+    : Store the argument in a variable named "branch"
+    SET branch=%1
+) ELSE (
+    : Else, clear the variable
+    SET branch=""
+)
+
+: Change to master, pull latest
 git checkout main && git pull origin main && git pull origin && git pull && git fetch
+
+: If a branch name was passed in
+IF NOT %branch%=="" (
+    : Store the argument in a variable named "branch"
+    git checkout %branch% && git pull origin %branch%
+)
+
+git branch
+
+@echo on
