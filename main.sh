fe80601073171a04c9c6572350801a837b08730b
The Jared Wilcurt <TheJaredWilcurt@users.noreply.github.com>
2024-10-24 10:22:57 -0400

    Update main.sh

--- main.sh
+++ main.sh
@@ -12,13 +12,13 @@ else
     branch=""
 fi
 
-# Change to main, pull latest
+# Change to main, pull latest, do not open the default editor
 git checkout main && git pull origin main --no-edit && git pull origin --no-edit && git pull --no-edit && git fetch
 
 # If a branch name was passed in
 if [ ! -z "$branch" ]
 then
-    # Switch to the desired branch and pull latest
+    # Switch to the desired branch and pull latest, do not open the default editor
     git checkout $branch && git pull origin $branch --no-edit
 fi
 
